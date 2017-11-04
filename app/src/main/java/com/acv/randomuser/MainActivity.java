e819b42a426c8d1e2a99eca014d6de19fd6dfc2b
carbaj0 <carbaj03@gmail.com>
2017-11-04 13:25:31 +0100

    Implements Main view
    Add some styles, color, dimens, fonts to customize the view

--- app/src/main/java/com/acv/randomuser/MainActivity.java
+++ app/src/main/java/com/acv/randomuser/MainActivity.java
@@ -1,23 +1,30 @@
 package com.acv.randomuser;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
-import javax.inject.Inject;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dagger.android.AndroidInjection;
-import dagger.android.AndroidInjector;
-import dagger.android.DispatchingAndroidInjector;
-import dagger.android.support.HasSupportFragmentInjector;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.rvRandomUser)
+    RecyclerView rvRandomUser;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AndroidInjection.inject(this);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Random Users");
     }
 
 
